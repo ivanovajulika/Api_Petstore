e1b33448ff91710ce082efdf483e48b88c9736a9
ivanovajulika <julika77@mail.ru>
2022-12-13 21:11:41 +0700

    created common methods

--- api.py
+++ api.py
@@ -1,65 +1,43 @@
 import requests
 import json
 
-# from requests_toolbelt.multipart.encoder import MultipartEncoder
 
-url = "https://petstore.swagger.io/v2"
+class Requests:
+    @staticmethod
+    def get(url, data=None, headers=None, cookies=None):
+        return Requests._send(url, data, headers, cookies, "GET")
 
+    def post(url, data=None, headers=None, cookies=None):
+        return Requests._send(url, data, headers, cookies, "POST")
 
-class Pet:
-    """Class Pet Methods"""
-    def __init__(self):
-        self.url = url
+    def put(url, data=None, headers=None, cookies=None):
+        return Requests._send(url, data, headers, cookies, "PUT")
 
-    def get_pet_by_id(self, id):
-        """GET/pet/{petId} Find pet by ID"""
-        response = requests.get(f"{url}/pet/{id}")
-        status = response.status_code
-        result = response.json()
-        return status, result
+    def delete(url, data=None, headers=None, cookies=None):
+        return Requests._send(url, data, headers, cookies, "DELETE")
 
-    def get_pet_by_status(self, params):
-        """GET/pet/findByStatus Finds Pets by status"""
-        response = requests.get(f"{url}/pet/findByStatus?status={params}")
-        status = response.status_code
-        result = response.json()
-        return status, result
+    @staticmethod
+    def _send(url, data, headers, cookies, method):
+        url = f"https://petstore.swagger.io/v2{url}"
 
-    def post_update_pet(self, id, data):
-        """POST/pet/{petId} Updates a pet in the store with form data"""
-        response = requests.post(f"{url}/pet/{id}", data=data)
-        status = response.status_code
-        result = response.json()
-        return status, result
+        if data is None:
+            data = {}
+        elif headers is None:
+            headers = {}
+        elif cookies is None:
+            cookies = {}
 
-    def put_update_pet(self, data, headers):
-        """PUT/pet Update an existing pet"""
-        response = requests.post(f"{url}/pet", data=json.dumps(data), headers=headers)
-        status = response.status_code
-        result = response.json()
-        return status, result
+        if method == "GET":
+            response = requests.get(url, params=data, headers=headers, cookies=cookies)
+        elif method == "POST":
+            response = requests.post(
+                url, data=json.dumps(data), headers=headers, cookies=cookies
+            )
+        elif method == "PUT":
+            response = requests.put(url, data=data, headers=headers, cookies=cookies)
+        elif method == "DELETE":
+            response = requests.delete(url, data=data, headers=headers, cookies=cookies)
+        else:
+            raise Exception(f"Very bad method '{method}'")
 
-    def post_add_new_pet(self, data, headers):
-        """POST/pet Add a new pet to the store"""
-        response = requests.post(f"{url}/pet", data=json.dumps(data), headers=headers)
-        status = response.status_code
-        result = response.json()
-        return status, result
-
-    def delete_pet(self, id, headers):
-        """DELETE/pet/{petId} Deletes a pet"""
-        response = requests.delete(f"{url}/pet/{id}", headers=headers)
-        status = response.status_code
-        result = response.json()
-        return status, result
-
-    """Here we will fix the method when we figure out how to send the file"""
-    # def post_uploads_image(self, id, pet_photo, headers):
-    #         """POST/pet/{petId}/uploadImage uploads an image"""
-    #         data = MultipartEncoder(
-    #             fields={'file': (pet_photo, open(pet_photo, 'rb'))})
-
-    #         response = requests.post(f"{url}/pet/{id}/uploadImage", data, headers)
-    #         status = response.status_code
-    #         result = response.json()
-    #         return status, result
+        return response
