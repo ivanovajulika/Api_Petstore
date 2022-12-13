255109bcaa8d392f2d9206ec0699c63a6ce01b25
ivanovajulika <julika77@mail.ru>
2022-12-13 21:59:40 +0700

    fixed yml

--- api_data.py
+++
@@ -1,8 +0,0 @@
-url = "https://petstore.swagger.io/v2"
-id = 879
-headers = {"accept": "application/json", "Content-Type": "application/json"}
-headers_mult = {"accept": "application/json", "Content-Type": "multipart/form-data"}
-data = {"id": 879, "name": "Anfisa", "status": "available"}
-update_data = {"name": "Murzik", "status": "sold"}
-update_data_2 = {"id": 879, "name": "Barsik", "status": "sold"}
-params = {"status": "available"}
