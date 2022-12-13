12da272324d3a42a55e3be3ca587219e8d181f2b
ivanovajulika <julika77@mail.ru>
2022-12-13 20:48:44 +0700

    created api project

---
+++ api.py
@@ -0,0 +1,65 @@
+import requests
+import json
+
+# from requests_toolbelt.multipart.encoder import MultipartEncoder
+
+url = "https://petstore.swagger.io/v2"
+
+
+class Pet:
+    """Class Pet Methods"""
+    def __init__(self):
+        self.url = url
+
+    def get_pet_by_id(self, id):
+        """GET/pet/{petId} Find pet by ID"""
+        response = requests.get(f"{url}/pet/{id}")
+        status = response.status_code
+        result = response.json()
+        return status, result
+
+    def get_pet_by_status(self, params):
+        """GET/pet/findByStatus Finds Pets by status"""
+        response = requests.get(f"{url}/pet/findByStatus?status={params}")
+        status = response.status_code
+        result = response.json()
+        return status, result
+
+    def post_update_pet(self, id, data):
+        """POST/pet/{petId} Updates a pet in the store with form data"""
+        response = requests.post(f"{url}/pet/{id}", data=data)
+        status = response.status_code
+        result = response.json()
+        return status, result
+
+    def put_update_pet(self, data, headers):
+        """PUT/pet Update an existing pet"""
+        response = requests.post(f"{url}/pet", data=json.dumps(data), headers=headers)
+        status = response.status_code
+        result = response.json()
+        return status, result
+
+    def post_add_new_pet(self, data, headers):
+        """POST/pet Add a new pet to the store"""
+        response = requests.post(f"{url}/pet", data=json.dumps(data), headers=headers)
+        status = response.status_code
+        result = response.json()
+        return status, result
+
+    def delete_pet(self, id, headers):
+        """DELETE/pet/{petId} Deletes a pet"""
+        response = requests.delete(f"{url}/pet/{id}", headers=headers)
+        status = response.status_code
+        result = response.json()
+        return status, result
+
+    """Here we will fix the method when we figure out how to send the file"""
+    # def post_uploads_image(self, id, pet_photo, headers):
+    #         """POST/pet/{petId}/uploadImage uploads an image"""
+    #         data = MultipartEncoder(
+    #             fields={'file': (pet_photo, open(pet_photo, 'rb'))})
+
+    #         response = requests.post(f"{url}/pet/{id}/uploadImage", data, headers)
+    #         status = response.status_code
+    #         result = response.json()
+    #         return status, result
---
+++ tests/tests_pet.py
@@ -0,0 +1,67 @@
+from api import Pet
+from api_data import *
+import os
+import pytest
+
+pet = Pet()
+
+""" You can run all tests at once by selecting a file
+pytest -s -v tests/tests_pet.py """
+
+
+def test_post_add_new_pet():
+    status, result = pet.post_add_new_pet(data, headers)
+    assert status == 200
+    assert result["name"] == data["name"]
+    print(result["name"])
+
+
+def test_get_pet_valid_id():
+    status, result = pet.get_pet_by_id(id)
+    assert status == 200
+    print(result)
+
+
+@pytest.mark.parametrize(
+    "params",
+    ["", "available", "pending", "sold"],
+    ids=["empty", "available", "pending", "sold"],
+)
+def test_get_pet_valid_status(params):
+    """Parameterization is used here, 4 tests will run"""
+    status, result = pet.get_pet_by_status(params=params)
+    assert status == 200
+    print(result)
+
+
+def test_post_update_pet():
+    status, result = pet.post_update_pet(id, data=update_data)
+    assert status == 200
+    status, result = pet.get_pet_by_id(id)
+    assert result["name"] == update_data["name"]
+    print(result["name"])
+
+
+def test_put_update_pet():
+    status, result = pet.put_update_pet(update_data_2, headers)
+    assert status == 200
+    status, result = pet.get_pet_by_id(id)
+    assert result["name"] == update_data_2["name"]
+    print(result["name"])
+
+
+def test_delete_pet():
+    status, result = pet.delete_pet(id, headers)
+    assert status == 200
+    assert int(result["message"]) == id
+    status, result = pet.get_pet_by_id(id)
+    assert status == 404
+
+
+"""Here we will fix the test when we figure out how to send the file"""
+# def test_uploads_image(pet_photo='images/cat1.jpg'):
+#     pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+#     headers = headers_mult
+#     status, result = pet.post_uploads_image(id, pet_photo, headers)
+#     assert status == 200
+#     print(result["message"])
