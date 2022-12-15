6b96c874e52a0e4aec8a90547352df70b27c70a5
Elena Ladygina <Ladygina_ev@yahoo.com>
2022-12-15 11:19:41 +0700

    added allure steps

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import allure
 
 # from requests_toolbelt.multipart.encoder import MultipartEncoder
 
@@ -17,42 +18,48 @@ class Pet:
         response = requests.get(f"{url}/pet/{id}")
         status = response.status_code
         result = response.json()
-        return status, result
+        with allure.step(f"GET request from url {response.request.path_url}"):
+            return status, result
 
     def get_pet_by_status(self, params):
         """GET/pet/findByStatus Finds Pets by status"""
         response = requests.get(f"{url}/pet/findByStatus?status={params}")
         status = response.status_code
         result = response.json()
-        return status, result
+        with allure.step(f"GET request from url {response.request.path_url}"):
+            return status, result
 
     def post_update_pet(self, id, data):
         """POST/pet/{petId} Updates a pet in the store with form data"""
         response = requests.post(f"{url}/pet/{id}", data=data)
         status = response.status_code
         result = response.json()
-        return status, result
+        with allure.step(f"POST request from url {response.request.path_url}"):
+            return status, result
 
     def put_update_pet(self, data, headers):
         """PUT/pet Update an existing pet"""
         response = requests.post(f"{url}/pet", data=json.dumps(data), headers=headers)
         status = response.status_code
         result = response.json()
-        return status, result
+        with allure.step(f"PUT request from url {response.request.path_url}"):
+            return status, result
 
     def post_add_new_pet(self, data, headers):
         """POST/pet Add a new pet to the store"""
         response = requests.post(f"{url}/pet", data=json.dumps(data), headers=headers)
         status = response.status_code
         result = response.json()
-        return status, result
+        with allure.step(f"POST request from url {response.request.path_url}"):
+            return status, result
 
     def delete_pet(self, id, headers):
         """DELETE/pet/{petId} Deletes a pet"""
         response = requests.delete(f"{url}/pet/{id}", headers=headers)
         status = response.status_code
         result = response.json()
-        return status, result
+        with allure.step(f"DELETE request from url {response.request.path_url}"):
+            return status, result
 
     """Here we will fix the method when we figure out how to send the file"""
     # def post_uploads_image(self, id, pet_photo, headers):
