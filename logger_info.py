1a5b3d8f30a0cacc73624995e337578c2553ba60
Elena Ladygina <Ladygina_ev@yahoo.com>
2022-12-16 16:13:59 +0700

    logger_info.py

--- logger_info.py
+++ logger_info.py
@@ -2,9 +2,9 @@ import logging
 
 logging.basicConfig(
     level="DEBUG",
-    filename='Mylog.log',
-    filemode='w',
-    format='%(asctime)s::%(levelname)s::%(message)s',
+    filename="Mylog.log",
+    filemode="w",
+    format="%(asctime)s::%(levelname)s::%(message)s",
 )
 
 logger = logging.getLogger()
